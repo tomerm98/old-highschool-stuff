0b0d37f22323e332bea163682ea01e562fd3937d
Tomre Mardan <tomer@mardan.co.il>
2017-01-16 14:47:33 +0200

    Project Created - Trump's Console Helper

---
+++ Trump's Console Helper/Trump's Console Helper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trump_s_Console_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
